48788c1f4050f9b31d79cfa386abe8f4c5ee648e
jz4o <26317360+jz4o@users.noreply.github.com>
2019-03-23 22:13:49 +0900

    rails/レコードと同時に子レコードを操作する を追加

---
+++ rails/レコードと同時に子レコードを操作する/sample_application/db/migrate/20190323020740_create_parents.rb
@@ -0,0 +1,10 @@
+class CreateParents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parents do |t|
+      t.string :code
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
